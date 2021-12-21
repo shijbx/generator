b6b5bf955854c0aa6535f121aef229885b8d344f
sain <sains@aiya.us>
2021-12-21 09:27:19 +0800

    update

--- src/main/java/cn/xiaobinbin/generator/CodeGeneration.java
+++ src/main/java/cn/xiaobinbin/generator/CodeGeneration.java
@@ -1,10 +1,10 @@
 package cn.xiaobinbin.generator;
 
+import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
 import com.baomidou.mybatisplus.generator.InjectionConfig;
 import com.baomidou.mybatisplus.generator.config.*;
 import com.baomidou.mybatisplus.generator.config.po.TableInfo;
-import com.baomidou.mybatisplus.generator.config.rules.DbType;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 
 import java.util.ArrayList;
@@ -12,7 +12,7 @@ import java.util.List;
 
 public class CodeGeneration {
 
-    public static final String path = FileUtils.readYml("gencode.outputUrl");
+    public static final String path = FileUtils.readYml("gencode.outputUrl") + "/src/main/java";
 
     public static final String packageName = FileUtils.readYml("gencode.packageName");
 
@@ -45,12 +45,20 @@ public class CodeGeneration {
     private static void getAllSetting(GlobalConfig gc) {
         // 全局配置
         gc.setOutputDir(path); // 输出文件路径
+        // 是否覆盖已有文件
+        gc.setFileOverride(false);
+        gc.setOpen(false);
         gc.setFileOverride(true);
-        gc.setActiveRecord(false); // 不需要ActiveRecord特性的请改为false
-        gc.setEnableCache(false); // XML 二级缓存
-        gc.setBaseResultMap(true); // XML ResultMap
-        gc.setBaseColumnList(true); // XML columList
-        gc.setAuthor(FileUtils.readYml("gencode.author")); // 作者
+        // 不需要ActiveRecord特性的请改为false
+        gc.setActiveRecord(false);
+        // XML 二级缓存
+        gc.setEnableCache(false);
+        // XML ResultMap
+        gc.setBaseResultMap(true);
+        // XML columList
+        gc.setBaseColumnList(true);
+        // 作者
+        gc.setAuthor(FileUtils.readYml("gencode.author"));
     }
 
     /**
@@ -94,8 +102,8 @@ public class CodeGeneration {
         String talbe = FileUtils.readYml("gencode.inTables");
         strategy.setInclude(talbe.split(",")); // 需要生成的表
         // 自定义实体父类
-        strategy.setSuperEntityClass(FileUtils.readYml("gencode.superEntityClass"));
-        String uperEntityColumns = FileUtils.readYml("gencode.uperEntityColumns");
+        strategy.setSuperEntityClass(FileUtils.readYml("system.superEntityClass"));
+        String uperEntityColumns = FileUtils.readYml("system.uperEntityColumns");
         // 自定义实体，公共字段
         strategy.setSuperEntityColumns(uperEntityColumns.split(","));
         // 自定义 mapper 父类
@@ -139,6 +147,8 @@ public class CodeGeneration {
                     }
                 });
         cfg.setFileOutConfigList(focList);
+
+
         mpg.setCfg(cfg);
     }
 
@@ -154,12 +164,13 @@ public class CodeGeneration {
         // 包配置/**/mpg.setTemplate(templateConfig);
         PackageConfig pc = new PackageConfig();
         pc.setParent(packageName);
+        pc.setEntity("entity");
         pc.setController("controller");
         pc.setService("service");
         pc.setServiceImpl("service.impl");
         pc.setMapper("mapper");
-        pc.setEntity("entity");
         pc.setXml("xml");
+
         mpg.setPackageInfo(pc);
     }
 
